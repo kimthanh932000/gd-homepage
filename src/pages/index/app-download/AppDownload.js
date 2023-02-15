79894e6f66b18adcc92e0530a0d7e5523b8e10a8
thanhtran <thanh.tran@kamora.vn>
2023-02-15 16:19:41 +0700

    section app

---
+++ src/pages/index/app-download/AppDownload.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './AppDownload.scss';
+import iconApple from '../../../assets/images/icon-apple-store.svg';
+import iconGoogle from '../../../assets/images/icon-google-store.svg';
+
+function AppDownload() {
+	return (
+		<section className="app">
+			<div className="container wrapper">
+				<h2 className="app__ttl">Get our App</h2>
+				<p className="app__txt">You can use our App for better experience on smartphones</p>
+				<div className="app-download">
+					<a className="app__link" href="#">
+						<img src={iconApple} alt="apple" width={60} height={60}/>
+						App Store
+					</a>
+					<a className="app__link" href="#">
+						<img src={iconGoogle} alt="google" width={60} height={60}/>
+						Google Play
+					</a>
+				</div>
+			</div>
+		</section>
+	);
+}
+
+export default AppDownload;
--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -1,6 +1,7 @@
 import Hero from "./hero/Hero";
 import Testimonials from "./testimonials/Testimonials";
 import Courses from "./courses/Courses";
+import AppDownload from "./app-download/AppDownload";
 
 export default function Index() {
 	return (
@@ -8,6 +9,7 @@ export default function Index() {
 			<Hero/>
 			<Testimonials/>
 			<Courses/>
+			<AppDownload/>
 		</>
 	);
 }
